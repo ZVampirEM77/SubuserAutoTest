094590db83e224aa36adad5792135db112b463cc
Enming Zhang <enming.zhang@umcloud.com>
2017-06-28 20:44:25 +0800

    add subuser delete && 1000 subuser test case
    Signed-off-by: Enming Zhang <enming.zhang@umcloud.com>

--- auto_test.py
+++ auto_test.py
@@ -11,7 +11,7 @@ import time
 import argparse
 import subprocess
 
-ceph_path = "../ceph/build/"
+ceph_path = "../../ceph/build/"
 py_dir = os.getcwd()
 
 def exec_command(command):
@@ -1805,6 +1805,230 @@ class TestCase16(object):
 
 	self.clean()
 
+class TestCase17(object):
+    def prepare(self):
+        os.chdir(ceph_path)
+        exec_command('./bin/radosgw-admin user create --uid=user1 --access-key=user1 --secret-key=user1 --display-name="user1"')
+        exec_command('./bin/radosgw-admin subuser create --key-type=s3 --uid=user1 --subuser=subuser1 --access=full --access-key=subuser1 --secret-key=subuser1')
+        exec_command('./bin/radosgw-admin subuser create --key-type=s3 --uid=user1 --subuser=subuser2 --access=full --access-key=subuser2 --secret-key=subuser2')
+        os.chdir(py_dir)
+
+    def op1(self):
+        exec_command('s3cmd -c user1.s3cfg mb s3://test1')
+        exec_command('s3cmd -c subuser1.s3cfg put user1.s3cfg s3://test1')
+        exec_command('s3cmd -c subuser2.s3cfg mb s3://test2') 
+        time.sleep(30)
+
+    def op2(self):
+        os.chdir(ceph_path)
+	exec_command('./bin/radosgw-admin subuser rm --uid=user1 --subuser=subuser1')
+        os.chdir(py_dir)
+
+    def op3(self):
+	os.chdir(ceph_path)
+	exec_command('./bin/radosgw-admin subuser rm --uid=user1 --subuser=subuser2')
+        os.chdir(py_dir)
+
+    def run(self):	
+        expect_dict1 = {"entries_size": 1,
+			"user1": {"test1": {"categories": {"create_bucket": {"ops": 1, "successful_ops": 1}, 
+				                           "put_obj": {"ops": 1, "successful_ops": 1}}},
+				  "test2": {"categories": {"create_bucket": {"ops": 1, "successful_ops": 1}}}}}
+        expect_dict2 = {"entries_size": 1,
+			"user1:subuser1": {"test1": {"categories": {"put_obj": {"ops": 1, "successful_ops": 1}}}}}
+        expect_dict3 = {"entries_size": 1,
+			"user1:subuser2": {"test2": {"categories": {"create_bucket": {"ops": 1, "successful_ops": 1}}}}}
+        
+        self.prepare()
+	self.op1()
+        result1 = result2 = result3 = False
+        os.chdir(ceph_path)
+        result1 = verify_show_response_msg('./bin/radosgw-admin usage show --uid=user1', expect_dict1)
+        result2 = verify_show_response_msg('./bin/radosgw-admin usage show --uid=user1 --subuser=subuser1', expect_dict2)
+        result3 = verify_show_response_msg('./bin/radosgw-admin usage show --uid=user1 --subuser=subuser2', expect_dict3)
+        os.chdir(py_dir)
+
+	if result1 == result2 == result3 == True:
+            self.op2()
+            expect_dict4 = expect_dict1
+	    expect_dict5 = expect_dict3
+            os.chdir(ceph_path)
+            result1 = verify_show_response_msg('./bin/radosgw-admin usage show --uid=user1', expect_dict4)
+            result2 = verify_show_response_msg('./bin/radosgw-admin usage show --uid=user1 --subuser=subuser2', expect_dict5)
+            os.chdir(py_dir)
+	    if result1 == result2 == True:
+	        self.op3()
+		expect_dict6 = expect_dict4
+                os.chdir(ceph_path)
+                result1 = verify_show_response_msg('./bin/radosgw-admin usage show --uid=user1', expect_dict4)
+                os.chdir(py_dir)
+		if result1 == True: 
+	            print "testcase17                         %s" % (ok_display("OK"))
+		else:
+		    print "testcase17                         %s" % (fail_display("FAIL"))
+            else:
+	        print "testcase17                         %s" % (fail_display("FAIL"))
+	else:
+	    print "testcase17                         %s" % (fail_display("FAIL"))
+	
+	self.clean()
+
+    def clean(self):
+        exec_command('s3cmd -c user1.s3cfg rb s3://test1 --recursive')
+        exec_command('s3cmd -c user1.s3cfg rb s3://test2')
+	time.sleep(30)
+        os.chdir(ceph_path)
+        exec_command('./bin/radosgw-admin usage trim --uid=user1')
+        exec_command('./bin/radosgw-admin user rm --uid=user1 --purge-data --purge-keys')
+        os.chdir(py_dir)
+
+class TestCase18(object):
+    def prepare(self):
+        os.chdir(ceph_path)
+        exec_command('./bin/radosgw-admin user create --uid=user1 --access-key=user1 --secret-key=user1 --display-name="user1"')
+        exec_command('./bin/radosgw-admin user create --uid=user2 --access-key=user2 --secret-key=user2 --display-name="user2"')
+	subuser_id = ''
+	command = ''
+	for i in range(1, 1001):
+	    subuser_id = 'subuser%d' % (i)
+	    command = './bin/radosgw-admin subuser create --key-type=s3 --uid=user1 --subuser={subuser} --access=full --access-key={subuser} --secret-key={subuser}'.format(subuser = subuser_id)
+            exec_command(command)
+        os.chdir(py_dir)
+
+    def op1(self):
+	exec_command('s3cmd -c user1.s3cfg mb s3://test1')
+	time.sleep(30)
+
+    def op2(self):
+	exec_command('s3cmd -c subuser1.s3cfg mb s3://test2')
+	time.sleep(30)
+
+    def op3(self):
+	exec_command('s3cmd -c subuser666.s3cfg put user1.s3cfg s3://test2')
+	time.sleep(30)
+
+    def op4(self):
+	exec_command('s3cmd -c user2.s3cfg mb s3://test3')
+        time.sleep(30)
+
+    def clean(self):
+	exec_command('s3cmd -c user1.s3cfg rb s3://test1')
+	exec_command('s3cmd -c user1.s3cfg rb s3://test2 --recursive')
+	exec_command('s3cmd -c user2.s3cfg rb s3://test3')
+	time.sleep(30)
+	os.chdir(ceph_path)
+	exec_command('./bin/radosgw-admin usage trim --uid=user1')
+	exec_command('./bin/radosgw-admin usage trim --uid=user2')
+        exec_command('./bin/radosgw-admin user rm --uid=user1 --purge-data --purge-keys')
+        exec_command('./bin/radosgw-admin user rm --uid=user2 --purge-data --purge-keys')
+        os.chdir(py_dir)
+
+    def run(self):
+        result1 = result2 = result3 = result4 = False
+        self.prepare()
+	self.op1()
+	expect_dict1 = {"entries_size": 1,
+			"user1": {"test1": {"categories": {"create_bucket": {"ops": 1, "successful_ops": 1}}}}}
+	expect_dict2 = {"entries_size": 0}
+        os.chdir(ceph_path)
+        result1 = verify_show_response_msg('./bin/radosgw-admin usage show --uid=user1', expect_dict1)
+        result2 = verify_show_response_msg('./bin/radosgw-admin usage show --uid=user2', expect_dict2)
+	subuser_id = ''
+	command = ''
+	for i in range(1, 1001):
+	    subuser_id = 'subuser%d' % (i)
+	    command = './bin/radosgw-admin usage show --uid=user1 --subuser={subuser}'.format(subuser = subuser_id)
+            result3 = verify_show_response_msg(command, expect_dict2)
+	    if result3 == False:
+	        break
+        os.chdir(py_dir)
+        if result1 == result2 == result3 == True:
+	    self.op2()
+	    expect_dict3 = {"entries_size": 1,
+			    "user1": {"test1": {"categories": {"create_bucket": {"ops": 1, "successful_ops": 1}}},
+				      "test2": {"categories": {"create_bucket": {"ops": 1, "successful_ops": 1}}}}}
+	    expect_dict4 = {"entries_size": 1,
+			    "user1:subuser1": {"test2": {"categories": {"create_bucket": {"ops": 1, "successful_ops": 1}}}}}
+	    expect_dict5 = expect_dict2
+	    os.chdir(ceph_path)
+	    result1 = verify_show_response_msg('./bin/radosgw-admin usage show --uid=user1', expect_dict3)
+	    result2 = verify_show_response_msg('./bin/radosgw-admin usage show --uid=user2', expect_dict5)
+	    for i in range(1, 1001):
+	        subuser_id = 'subuser%d' % (i)
+	        command = './bin/radosgw-admin usage show --uid=user1 --subuser={subuser}'.format(subuser = subuser_id)
+		if i == 1:
+		    result3 = verify_show_response_msg('./bin/radosgw-admin usage show --uid=user1 --subuser=subuser1', expect_dict4)
+		else:
+		    result3 = verify_show_response_msg(command, expect_dict5)
+
+	        if result3 == False:
+		    break
+            os.chdir(py_dir)
+	    if result1 == result2 == result3 == True:
+                self.op3()
+		expect_dict6 = {"entries_size": 1,
+			        "user1": {"test1": {"categories": {"create_bucket": {"ops": 1, "successful_ops": 1}}},
+				          "test2": {"categories": {"create_bucket": {"ops": 1, "successful_ops": 1},
+						                   "put_obj": {"ops": 1, "successful_ops": 1}}}}}
+		expect_dict7 = expect_dict4
+		expect_dict8 = {"entries_size": 1,
+			        "user1:subuser666": {"test2": {"categories": {"put_obj": {"ops": 1, "successful_ops": 1}}}}}
+		expect_dict9 = expect_dict5
+	        os.chdir(ceph_path)
+	        result1 = verify_show_response_msg('./bin/radosgw-admin usage show --uid=user1', expect_dict6)
+	        result2 = verify_show_response_msg('./bin/radosgw-admin usage show --uid=user2', expect_dict9)
+	        for i in range(1, 1001):
+	            subuser_id = 'subuser%d' % (i)
+	            command = './bin/radosgw-admin usage show --uid=user1 --subuser={subuser}'.format(subuser = subuser_id)
+		    if i == 1:
+		        result3 = verify_show_response_msg(command, expect_dict7)
+		    elif i == 666:
+                        result3 = verify_show_response_msg(command, expect_dict8)
+		    else:
+		        result3 = verify_show_response_msg(command, expect_dict9)
+
+	            if result3 == False:
+		        break
+                os.chdir(py_dir)
+		if result1 == result2 == result3 == True:
+	            self.op4()
+		    expect_dict10 = expect_dict6
+		    expect_dict11 = expect_dict7
+		    expect_dict12 = expect_dict8
+		    expect_dict13 = {"entries_size": 1,
+			             "user2": {"test3": {"categories": {"create_bucket": {"ops": 1, "successful_ops": 1}}}}}
+		    expect_dict14 = expect_dict9
+                    os.chdir(ceph_path)
+	            result1 = verify_show_response_msg('./bin/radosgw-admin usage show --uid=user1', expect_dict10)
+	            result2 = verify_show_response_msg('./bin/radosgw-admin usage show --uid=user2', expect_dict13)
+	            for i in range(1, 1001):
+	                subuser_id = 'subuser%d' % (i)
+	                command = './bin/radosgw-admin usage show --uid=user1 --subuser={subuser}'.format(subuser = subuser_id)
+		        if i == 1:
+		            result3 = verify_show_response_msg(command, expect_dict11)
+		        elif i == 666:
+                            result3 = verify_show_response_msg(command, expect_dict12)
+		        else:
+		            result3 = verify_show_response_msg(command, expect_dict14)
+
+	                if result3 == False:
+		            break
+                    os.chdir(py_dir)
+		    if result1 == result2 == result3 == True:
+		        print "testcase18                         %s" % (ok_display("OK"))
+		    else:
+		        print "testcase18                         %s" % (fail_display("FAIL"))
+		else:
+		    print "testcase18                         %s" % (fail_display("FAIL"))
+            else:
+                print "testcase18                         %s" % (fail_display("FAIL"))
+        else:
+            print "testcase18                         %s" % (fail_display("FAIL"))
+
+	self.clean()
+
+def TestCase19(object):
+    
 
 if __name__ == '__main__': 
     test_case1 = TestCase1()
@@ -1839,3 +2063,7 @@ if __name__ == '__main__':
     test_case15.run()
     test_case16 = TestCase16()
     test_case16.run()
+    test_case17 = TestCase17()
+    test_case17.run()
+    test_case18 = TestCase18()
+    test_case18.run()
